352b6e7a9f88aeb20f57a91c336f6671190bb174
Wojciech Pasiński <wpasinski@gmail.com>
2016-10-11 11:43:49 +0200

    [DASH-2084] Add missing slashes in paths

--- src/utils/RoutesUtil.js
+++ src/utils/RoutesUtil.js
@@ -68,7 +68,7 @@ const RoutesUtil = {
     }
 
     let name = 'app';
-    const names = nextState.routes.map((route) => route.name).filter((routeName) => typeof routeName !== 'undefined');
+    const names = nextState.routes.map(route => route.name).filter(routeName => typeof routeName !== 'undefined');
 
     if (names.length > 0) {
       name = names[names.length - 1];
@@ -123,7 +123,7 @@ const RoutesUtil = {
 
     if (lastInstanceName) {
       this.isInstanceAvailable(lastInstanceName)
-        .then(replace({ pathname: `/instances/${lastInstanceName}/sockets` }));
+        .then(replace({ pathname: `/instances/${lastInstanceName}/sockets/` }));
     }
   },
 
