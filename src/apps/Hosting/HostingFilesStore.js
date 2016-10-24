7870295d46c7aadb7be3f0964d555516c6694a50
Wojciech Pasiński <wpasinski@gmail.com>
2016-10-24 11:21:14 +0200

    [DASH-2187] Add /sockets/ to instance list items paths

--- src/apps/Hosting/HostingFilesStore.js
+++ src/apps/Hosting/HostingFilesStore.js
@@ -64,7 +64,7 @@ export default Reflux.createStore({
     currentHostingId && Actions.fetchFiles(currentHostingId);
   },
 
-  setHostingId(hostingId) {
+  onSetHostingId(hostingId) {
     this.data.currentHostingId = hostingId;
   },
 
