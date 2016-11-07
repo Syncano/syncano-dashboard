e8784935924ae51926331e181af77b1e0c12765a
Daniel <danielsiwek95@gmail.com>
2016-11-07 17:21:30 +0100

    [DASH-2363] Hosting Go To Url button should work with urls created from name

--- src/apps/Hosting/Files.js
+++ src/apps/Hosting/Files.js
@@ -73,7 +73,7 @@ const HostingFilesView = React.createClass({
     const { instanceName } = this.props.params;
     const defaultHostingUrl = `https://${instanceName}.syncano.site/`;
     const hasDomains = hostingDetails && hostingDetails.domains.length > 0;
-    const customDomainUrl = hasDomains ? `https://${instanceName}--${hostingDetails.domains[0]}.syncano.site/` : null;
+    const customDomainUrl = hasDomains ? `https://${hostingDetails.domains[0]}--${instanceName}.syncano.site/` : null;
     const hostingUrl = this.isDefaultHosting() ? defaultHostingUrl : customDomainUrl;
 
     return hostingUrl;
