2a0639f858090f6f8d3053b4dce740f507e956d3
Wojciech Pasiński <wpasinski@gmail.com>
2016-11-10 14:22:47 +0100

    [DASH-2356] Invited admin can 'delete' a shared instance

--- src/common/Dialog/DialogStandardButtons.js
+++ src/common/Dialog/DialogStandardButtons.js
@@ -1,43 +1,52 @@
 import React from 'react';
 import _ from 'lodash';
+
 import { FlatButton, RaisedButton } from 'material-ui';
 
 const DialogStandardButtons = ({
   handleCancel,
   handleConfirm,
-  submitLabel,
-  cancelLabel,
+  submitLabel = 'Confirm',
+  cancelLabel = 'Cancel',
+  inverted,
   disabled,
   submitDisabled,
   cancelDisabled,
   ...other
-}) => (
-  <div>
-    <FlatButton
-      data-e2e={other['data-e2e-cancel']}
-      style={{ marginRight: 10 }}
-      key="cancel"
-      label={cancelLabel}
-      onTouchTap={_.debounce(handleCancel, 500, { leading: true })}
-      disabled={disabled || cancelDisabled}
-    />
-    <RaisedButton
-      data-e2e={other['data-e2e-submit']}
-      key="confirm"
-      label={submitLabel}
-      primary={true}
-      onTouchTap={_.debounce(handleConfirm, 500, { leading: true })}
-      disabled={disabled || submitDisabled}
-    />
-  </div>
-);
+}) => {
+  const styles = {
+    rootInverted: {
+      display: 'flex',
+      flexDirection: 'row-reverse',
+      justifyContent: 'flex-end',
+      marginLeft: -10
+    },
+    button: {
+      marginLeft: 10
+    }
+  };
 
-DialogStandardButtons.defaultProps = {
-  submitLabel: 'Confirm',
-  cancelLabel: 'Cancel',
-  disabled: false,
-  submitDisabled: false,
-  cancelDisabled: false
+  return (
+    <div style={inverted && styles.rootInverted}>
+      <FlatButton
+        key="cancel"
+        label={cancelLabel}
+        style={styles.button}
+        disabled={disabled || cancelDisabled}
+        onTouchTap={_.debounce(handleCancel, 500, { leading: true })}
+        data-e2e={other['data-e2e-cancel']}
+      />
+      <RaisedButton
+        key="confirm"
+        label={submitLabel}
+        primary={true}
+        style={styles.button}
+        disabled={disabled || submitDisabled}
+        onTouchTap={_.debounce(handleConfirm, 500, { leading: true })}
+        data-e2e={other['data-e2e-submit']}
+      />
+    </div>
+  );
 };
 
 export default DialogStandardButtons;
