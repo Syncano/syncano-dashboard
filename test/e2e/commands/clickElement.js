00b124bdacdb5af3b7dc957f5028d58ce7ce5856
Adam Wardecki <adam.wardecki@gmail.com>
2016-11-16 13:47:38 +0100

    [DASH-2398] Removing pauses is apparently a bad idea

--- test/e2e/commands/clickElement.js
+++ test/e2e/commands/clickElement.js
@@ -5,5 +5,6 @@ exports.command = function clickElement(element) {
   return this
     .waitForElementPresent(element)
     .moveToElement(element, 0, 0)
-    .click(element);
+    .click(element)
+    .pause(1000);
 };
