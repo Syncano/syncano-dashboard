2af627ba104be1588c294967bad92a2adc649b07
Marcin Godlewski <gh3r@users.noreply.github.com>
2016-11-16 15:35:19 +0100

    [DASH-2401] Remove commented out code

--- src/pages/instance.jsx
+++ src/pages/instance.jsx
@@ -78,20 +78,6 @@ const Instance = React.createClass({
                   iconClassName="synicon-cellphone-iphone"
                   initiallyOpen={true}
                   autoGenerateNestedIndicator={false}
-                  /*
-                  nestedItems={[
-                  <Sidebar.NestedLinkListItem
-                  key="iOSDevices"
-                  routeName="apns-devices"
-                  primaryText="iOS Devices"
-                  />,
-                  <Sidebar.NestedLinkListItem
-                  key="androidDevices"
-                  routeName="gcm-devices"
-                  primaryText="Android Devices"
-                  />
-                  ]}
-                  */
                 />
 
                 <Sidebar.LinkListItem
@@ -102,20 +88,6 @@ const Instance = React.createClass({
                   iconClassName="synicon-cellphone-iphone"
                   initiallyOpen={true}
                   autoGenerateNestedIndicator={false}
-                  /*
-                  nestedItems={[
-                  <Sidebar.NestedLinkListItem
-                  key="iOSMessages"
-                  routeName="apns-messages"
-                  primaryText="iOS Messages"
-                  />,
-                  <Sidebar.NestedLinkListItem
-                  key="androidMessages"
-                  routeName="gcm-messages"
-                  primaryText="Android Messages"
-                  />
-                  ]}
-                  */
                 />
 
               </Sidebar.List>
