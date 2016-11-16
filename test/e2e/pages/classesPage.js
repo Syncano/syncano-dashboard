4b0beda4f602f48870cb77d6c825e95367105d02
Tomasz Ciecierski <tomek.ciecierski@syncano.com>
2016-11-16 09:50:00 +0100

    [DASH-2376] Add some selectors in order to not crash other tests

--- test/e2e/pages/classesPage.js
+++ test/e2e/pages/classesPage.js
@@ -1,7 +1,52 @@
+import utils from '../utils';
+
 export default {
   elements: {
+    classesListMenu: {
+      selector: '//div[@class="classes-list"]/div[1]/div[@class="col-menu"]//button',
+      locateStrategy: 'xpath'
+    },
+    createModalNameInput: {
+      selector: 'input[name=class]'
+    },
+    createModalFieldNameInput: {
+      selector: 'input[name=fieldName]'
+    },
+    createModalDropdownType: {
+      selector: '//div[@class="fieldType-dropdown"]/div/div',
+      locateStrategy: 'xpath'
+    },
+    createModalDescriptionInput: {
+      selector: 'input[name="description"]'
+    },
     classTableRows: {
       selector: '//div[@class="classes-list"]/div[2]/div',
+      locateStrategy: 'xpath'
+    },
+    classTableRow: {
+      selector: `//div[text()="${utils.addSuffix('class')}"]`,
+      locateStrategy: 'xpath'
+    },
+    userProfileClassName: {
+      selector: '[data-e2e="user_profile-list-item-name"]'
+    },
+    userClassListItem: {
+      selector: '[data-e2e="user_profile-check-icon"]'
+    },
+    classTableRowDescription: {
+      selector: `div[data-e2e="${utils.addSuffix('class')}-list-item-description"]`
+    },
+    classTableName: {
+      selector: `div[data-e2e="${utils.addSuffix('class')}-list-item-name"]`
+    },
+
+    checkboxSelected: {
+      selector: '//span[@class="synicon-checkbox-marked-outline"]',
+      locateStrategy: 'xpath'
+    },
+    classesListItemDropDown: {
+      selector: `//div[text()="${utils.addSuffix('class')}"]/../../../
+        following-sibling::div//span[@class="synicon-dots-vertical"]`,
       locateStrategy: 'xpath'
     }
   }
