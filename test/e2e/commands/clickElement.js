38c0314a92d9de2b9d4fd81802bafd98869c97d6
Adam Wardecki <adam.wardecki@gmail.com>
2016-11-16 13:13:20 +0100

    [DASH-2398] Remove pause from move to element command

--- test/e2e/commands/clickElement.js
+++ test/e2e/commands/clickElement.js
@@ -5,6 +5,5 @@ exports.command = function clickElement(element) {
   return this
     .waitForElementPresent(element)
     .moveToElement(element, 0, 0)
-    .click(element)
-    .pause(1000);
+    .click(element);
 };
