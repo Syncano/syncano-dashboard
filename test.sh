35e3964a34bb1a69a9efb43e731ace21973ecab0
Patryk Kopycinski <contact@patrykkopycinski.com>
2016-12-05 15:24:29 +0100

    [DASH-2413] Test

--- test.sh
+++ test.sh
@@ -45,7 +45,7 @@ function ci_cleanup {
 function ci_setup {
     selenium_install
 
-    npm run build:staging
+    npm run build:test
     mv ./dist ./dist_e2e
 
     babel-node ./test/setup/createTestInstances.js
