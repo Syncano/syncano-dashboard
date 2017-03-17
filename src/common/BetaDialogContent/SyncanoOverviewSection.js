a67d81ad7e8bc66a5b833a5954be47276ca00f2b
Daniel <danielsiwek95@gmail.com>
2017-03-17 16:59:04 +0100

    [SYN5-243] Add alt tags

--- src/common/BetaDialogContent/SyncanoOverviewSection.js
+++ src/common/BetaDialogContent/SyncanoOverviewSection.js
@@ -32,14 +32,14 @@ const SyncanoOverviewSection = () => {
     <div style={styles.root}>
       <div style={styles.rowContainer}>
         <div style={styles.box}>
-          <img src={require('../../assets/img/features/socket.svg')} alt="" />
+          <img src={require('../../assets/img/features/socket.svg')} alt="socket icon" />
           <h2 style={styles.title}>Custom Sockets</h2>
           <p style={styles.description}>
             Reusable building blocks for a backend development
           </p>
         </div>
         <div style={styles.box}>
-          <img src={require('../../assets/img/features/sockets.svg')} alt="" />
+          <img src={require('../../assets/img/features/sockets.svg')} alt="sockets icon" />
           <h2 style={styles.title}>Private Sockets registry</h2>
           <p style={styles.description}>
             Store with thounsends of backend solutions
@@ -48,14 +48,14 @@ const SyncanoOverviewSection = () => {
       </div>
       <div style={styles.rowContainer}>
         <div style={styles.box}>
-          <img src={require('../../assets/img/features/terminal.svg')} alt="" />
+          <img src={require('../../assets/img/features/terminal.svg')} alt="terminal icon" />
           <h2 style={styles.title}>Powerful CLI</h2>
           <p style={styles.description}>
             Tight interation with developer enviroment to control whole process of building server-side like front-end
           </p>
         </div>
         <div style={styles.box}>
-          <img src={require('../../assets/img/features/hosting.svg')} alt="" />
+          <img src={require('../../assets/img/features/hosting.svg')} alt="hosting icon" />
           <h2 style={styles.title}>Hosting</h2>
           <p style={styles.description}>
             Fast and secure static hosting for your web app (free SSL certificate included!)
@@ -64,14 +64,14 @@ const SyncanoOverviewSection = () => {
       </div>
       <div style={styles.rowContainer}>
         <div style={styles.box}>
-          <img src={require('../../assets/img/features/database.svg')} alt="" />
+          <img src={require('../../assets/img/features/database.svg')} alt="database icon" />
           <h2 style={styles.title}>NoSQL realtime database</h2>
           <p style={styles.description}>
             Store and sync data with NoSQL highly scalable database
           </p>
         </div>
         <div style={styles.box}>
-          <img src={require('../../assets/img/features/users.svg')} alt="" />
+          <img src={require('../../assets/img/features/users.svg')} alt="users icon" />
           <h2 style={styles.title}>User Management</h2>
           <p style={styles.description}>
             Store users, identity authenticate user with password or using identity providers like Facebook or Twitter
