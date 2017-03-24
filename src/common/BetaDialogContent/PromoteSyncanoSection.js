a328ea1abd6677691bf7892e21334854be05de51
Daniel <danielsiwek95@gmail.com>
2017-03-24 19:18:38 +0100

    [SYN5-301] Add support for gmail subadresses like ‘test+1@gmail.com’ [e2e-skip]

--- src/common/BetaDialogContent/PromoteSyncanoSection.js
+++ src/common/BetaDialogContent/PromoteSyncanoSection.js
@@ -21,7 +21,7 @@ const PromoteSyncanoSection = React.createClass({
 
       emails.forEach((email, index, arr) => {
         // eslint-disable-next-line
-        const emailRegex = new RegExp("^[a-z][a-zA-Z0-9_.]*(\.[a-zA-Z][a-zA-Z0-9_.]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$", 'g');
+        const emailRegex = new RegExp("^[a-z][a-zA-Z0-9_.+]*(\.[a-zA-Z][a-zA-Z0-9_.]*)?@[a-z][a-zA-Z-0-9]*\.[a-z]+(\.[a-z]+)?$", 'g');
         const isValidEmail = emailRegex.test(email);
 
         if (!isValidEmail) {
