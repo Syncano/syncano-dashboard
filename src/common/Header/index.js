a7bea534fed3e8e2ccfff54545c05fd6746ed9e4
Adam Wardecki <adam.wardecki@syncano.com>
2017-04-14 17:02:31 +0200

    [SYN5-466] Hide the notifications dropdown

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,10 +1,8 @@
 import Header from './Header';
 import HeaderInstancesDropdown from './HeaderInstancesDropdown';
-import HeaderNotificationsDropdown from './HeaderNotificationsDropdown';
 import InvitationItem from './InvitationItem';
 
 Header.InstancesDropdown = HeaderInstancesDropdown;
-Header.NotificationsDropdown = HeaderNotificationsDropdown;
 Header.InvitationItem = InvitationItem;
 
 export default Header;
